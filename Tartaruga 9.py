60004e78c83f0b3200df096bb505aa88024f7bdc
LuanRamalho <32373552+LuanRamalho@users.noreply.github.com>
2021-04-23 19:30:30 -0300

    Add files via upload

---
+++ Tartaruga 9.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import turtle
+a = turtle.Screen()
+tartaruga = turtle.Turtle()
+tartaruga.shape("turtle")
+tartaruga.setx(100)
+tartaruga.sety(300)
+tartaruga.home()
+a.exitonclick()
